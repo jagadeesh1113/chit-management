73329244e402d23adad6827d5cc4a6d52cd8a984
Jagadeesh Y M <jagadeesh1113@gmail.com>
2026-02-11 20:56:30 +0530

    Remove sign in and sign up in header

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import { Button } from "./ui/button";
 import { LogoutButton } from "./logout-button";
 import { ThemeSwitcher } from "./theme-switcher";
 import { useAuth } from "@/context/AuthContext";
@@ -13,14 +11,5 @@ export function AuthButton() {
       <ThemeSwitcher />
       <LogoutButton />
     </div>
-  ) : (
-    <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/auth/login">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/auth/sign-up">Sign up</Link>
-      </Button>
-    </div>
-  );
+  ) : null;
 }
